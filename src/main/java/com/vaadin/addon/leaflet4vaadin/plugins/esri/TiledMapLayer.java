773486a1c000558e0455b14bbceec414c7898f70
Felipe Lang <felipe.lang@ext.vaadin.com>
2023-12-14 14:36:24 -0300

    refactor: change superclass and add lombok annotations

--- src/main/java/com/vaadin/addon/leaflet4vaadin/plugins/esri/TiledMapLayer.java
+++ src/main/java/com/vaadin/addon/leaflet4vaadin/plugins/esri/TiledMapLayer.java
@@ -1,35 +1,23 @@
 package com.vaadin.addon.leaflet4vaadin.plugins.esri;
 
 import com.vaadin.addon.leaflet4vaadin.annotations.LeafletArgument;
-import com.vaadin.addon.leaflet4vaadin.layer.Layer;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.dependency.NpmPackage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * <a href="https://github.com/Esri/esri-leaflet">Esri</a>
  * <a href="https://developers.arcgis.com/esri-leaflet/api-reference/layers/tiled-map-layer/">Tiled
  * map layer</a> plugin.  Access tiles from ArcGIS Online and ArcGIS Server.
  */
-@NpmPackage(value = "esri-leaflet", version = "3.0.12")
-@JsModule("esri-leaflet/dist/esri-leaflet.js")
 @JsModule("./esri-tiled-map-layer.js")
-public class TiledMapLayer extends Layer {
+@AllArgsConstructor
+public class TiledMapLayer extends EsriLeafletLayer {
 
   private static final long serialVersionUID = -7906787297109085580L;
   
+  @Getter
   @LeafletArgument
-  private TiledMapLayerOptions options;
-
-  public TiledMapLayer(TiledMapLayerOptions options) {
-    this.options = options;
-  }
-  
-  public void setOptions(TiledMapLayerOptions options) {
-    this.options = options;
-  }
-  
-  public TiledMapLayerOptions getOptions() {
-    return options;
-  }
+  private final TiledMapLayerOptions options;
   
 }
