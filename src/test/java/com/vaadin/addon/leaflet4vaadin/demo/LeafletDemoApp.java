9812b2ddaa43c3ee507e7f081ceb64aea49bcdd9
Paola De Bartolo <paola.debartolo@gmail.com>
2023-12-19 11:39:14 -0300

    refactor(demo): remove links from navbar

--- src/test/java/com/vaadin/addon/leaflet4vaadin/demo/LeafletDemoApp.java
+++ src/test/java/com/vaadin/addon/leaflet4vaadin/demo/LeafletDemoApp.java
@@ -20,32 +20,58 @@ import com.vaadin.addon.leaflet4vaadin.demo.view.controls.ControlPositionExample
 import com.vaadin.addon.leaflet4vaadin.demo.view.controls.LayersControlExample;
 import com.vaadin.addon.leaflet4vaadin.demo.view.controls.RemoveDefaultControlsExample;
 import com.vaadin.addon.leaflet4vaadin.demo.view.controls.ScaleControlExample;
-import com.vaadin.addon.leaflet4vaadin.demo.view.layers.*;
-import com.vaadin.addon.leaflet4vaadin.demo.view.map.*;
-import com.vaadin.addon.leaflet4vaadin.demo.view.marker.*;
+import com.vaadin.addon.leaflet4vaadin.demo.view.layers.GeoJSONEventsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.layers.GeoJSONFilterExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.layers.GeoJSONLayerExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.layers.GeoJSONStyleExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.layers.MultipleBaseLayersExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapConversionMethodsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapDarkThemeExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapDialogExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapEventsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapFunctionsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapGeolocationExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapMaxBoundsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapPollListenerExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MapStateFuncionsExmple;
+import com.vaadin.addon.leaflet4vaadin.demo.view.map.MultipleMapsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.DivOverlayStyleExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkerDivIconExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkerMethodCallExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkersAddAndRemoveExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkersChangingIconExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkersEventsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkersGroupExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkersRemoveOnClickExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkersSimpleExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.marker.MarkersWithEventsExample;
 import com.vaadin.addon.leaflet4vaadin.demo.view.mixed.WorldMapFlagsExample;
-import com.vaadin.addon.leaflet4vaadin.demo.view.path.*;
-import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.*;
-import com.vaadin.flow.component.HtmlContainer;
+import com.vaadin.addon.leaflet4vaadin.demo.view.path.FlyToPolygonBoundsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.path.PathSimpleExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.path.Paths3000Example;
+import com.vaadin.addon.leaflet4vaadin.demo.view.path.PathsEventPropagationExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.path.PathsStyleExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.path.TypeOfPathsExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.CanvasIconLayerExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.DynamicMapLayerPluginExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.FullScreenPluginExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.HeatmapPluginExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.KmzLayerPluginExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.MarkerClusterPluginExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.TiledMapLayerPluginExample;
+import com.vaadin.addon.leaflet4vaadin.demo.view.plugins.VectorBasemapLayerPluginExample;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
-
-import static com.vaadin.addon.leaflet4vaadin.demo.components.AppMenuItem.NEW;
 
 @CssImport(value = "styles/demo-applayout.css", themeFor = "vaadin-app-layout")
 @Theme(value = Lumo.class, variant = Lumo.LIGHT)
@@ -75,43 +101,8 @@ public class LeafletDemoApp extends AppLayout implements AfterNavigationObserver
 		appTitle.getStyle().set("font-weight", "300");
 		addToNavbar(appTitle);
 
-		// External links in navbar
-		initActionButtons();
-
 		// Application menubar
 		initializeDemoMenu();
-	}
-
-	private void initActionButtons() {
-		HorizontalLayout actions = new HorizontalLayout();
-		actions.setPadding(true);
-		actions.setWidthFull();
-		actions.setJustifyContentMode(JustifyContentMode.END);
-
-		HtmlContainer donate = new Div();
-		String donateForm = "<form action=\"https://www.paypal.com/cgi-bin/webscr\" "
-				+ "method=\"post\" target=\"_top\" style=\"display:flex; align-items:center; height: 100%; \">"
-				+ "<input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\" />"
-				+ "<input type=\"hidden\" name=\"hosted_button_id\" value=\"8M9BEK428U6AW\" />"
-				+ "<button type=\"submit\" style=\"border: none;font-size: 16px;color:#083469;font-weight:600;border:1px #e89740 solid; "
-				+ "box-shadow:0px 0px 5px #c1c1c1;padding: 8px 20px;background:linear-gradient(#f8e8a4,#f3a726);cursor: pointer;\" "
-				+ "title=\"PayPal - The safer, easier way to pay online!\">Donate</button>" + "</form>";
-		donate.getElement().setProperty("innerHTML", donateForm);
-
-		// Vaadin directory button
-		Button directoryButton = new Button("Vaddin directory", new Icon(VaadinIcon.VAADIN_H));
-		directoryButton.addClickListener(
-				(e) -> UI.getCurrent().getPage().setLocation("https://vaadin.com/directory/component/leaflet4vaadin"));
-		directoryButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY, ButtonVariant.LUMO_ERROR);
-
-		// Github button
-		Button githubButton = new Button("Github");
-		githubButton.addClickListener(
-				(e) -> UI.getCurrent().getPage().setLocation("https://github.com/Gubancs/leaflet4vaadin"));
-		githubButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		actions.add(directoryButton, githubButton, donate);
-
-		addToNavbar(actions);
 	}
 
 	private void initializeDemoMenu() {
