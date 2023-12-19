3e74e655c9b45e7d4e7534e87dd205e7a2be990b
Paola De Bartolo <paola.debartolo@gmail.com>
2023-12-19 11:40:49 -0300

    chore: remove unused imports from DynamicMapLayer

--- src/main/java/com/vaadin/addon/leaflet4vaadin/plugins/esri/DynamicMapLayer.java
+++ src/main/java/com/vaadin/addon/leaflet4vaadin/plugins/esri/DynamicMapLayer.java
@@ -1,10 +1,6 @@
 package com.vaadin.addon.leaflet4vaadin.plugins.esri;
 
 import com.vaadin.addon.leaflet4vaadin.annotations.LeafletArgument;
-import com.vaadin.addon.leaflet4vaadin.layer.events.LeafletEvent;
-import com.vaadin.addon.leaflet4vaadin.layer.events.LeafletEventListener;
-import com.vaadin.addon.leaflet4vaadin.layer.events.types.EventTypeRegistry;
-import com.vaadin.addon.leaflet4vaadin.layer.events.types.LeafletEventType;
 import com.vaadin.flow.component.dependency.JsModule;
 import lombok.Getter;
 
